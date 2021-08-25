3814d5e35702ccf9a7c2cbf3e25b4c5c8c761ec5
Yoan <62288669+yoancheck@users.noreply.github.com>
2021-08-26 02:04:50 +0300

    Add files via upload

---
+++ src/app/app-header/app-header.component.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-app-header',
+  templateUrl: './app-header.component.html',
+  styleUrls: ['./app-header.component.css']
+})
+export class AppHeaderComponent implements OnInit {
+  title = 'Earth Countries';
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
